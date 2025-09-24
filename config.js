e929ebd3d42b0a1e843d6d28d1e0a8435cc7693d
nlcc-apps <app@nlcc.ng>
2025-09-24 14:56:11 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,78 @@
+// config.js
+  const slideData = [
+  {
+    img: "images/joiners-Soye.jpg",
+    title: "WEBBOT: ENHANCING PATIENT ENGAGEMENT WITH TAWK.TO CHAT APP",
+    desc: "Streamline patient support with the robust Tawk.to chat app integration, offering 24/7 assistance.",
+  },
+  {
+    img: "images/banner-8.jpg",
+    title: "UNIFIED IT SERVICE MANAGEMENT: OPTIMIZING OPERATIONAL EFFICIENCY",
+    desc: "Elevate IT service delivery with a unified platform, featuring incident tracking and real-time booking.",
+  },
+  {
+    img: "images/revenue.png",
+    title: "SPECIAL/WEEKEND ACCESS TO MLCC: SECURE ENTRY PROTOCOLS UNVEILED",
+    desc: "Ensure secure access to MLCC with advanced protocols for special/weekend entry requests.",
+  },
+  {
+    img: "https://i.ibb.co/cchghPYV/banner-3.jpg",
+    title: "FAULT-TOLERANT ARCHITECTURE: ENSURING 100% SERVER REDUNDANCY",
+    desc: "Guarantee uninterrupted service with a fault-tolerant system mirroring data in real-time.",
+  },
+  {
+    img: "images/banner-6.jpg",
+    title: "MOUSE PROGRAMMING: AUTOMATED LICENSE MANAGEMENT FOR ARIA",
+    desc: "Automate license administration for ARIA with precision through innovative mouse programming.",
+  },
+  {
+    img: "images/banner-4.jpg",
+    title: "SURVIVORSHIP & REVENUE TRACKER: ANALYZING CONSULTATION METRICS",
+    desc: "Track survivorship and revenue with detailed consultation statistics and exportable reports.",
+  },
+  {
+    img: "images/visits.png",
+    title: "CONTACTLESS PAYMENT VALIDATION: INTELLIGENT MIDDLEWARE SOLUTIONS",
+    desc: "Implement smart middleware for seamless, non-intrusive payment validation with audit alerts.",
+  },
+  {
+    img: "images/invoice-validation.jpg",
+    title: "PAYMENT VALIDATION MIDDLEWARE: REAL-TIME TRANSACTION VALIDATOR",
+    desc: "Validate transactions in real-time with a back-end agent, ensuring end-to-end payment integrity.",
+  },
+  {
+    img: "images/email-access.jpg",
+    title: "AI PORTAL: ADVANCED PATIENT SUPPORT AND SELF-SERVICE HUB",
+    desc: "Offer patients a smart AI portal with video support, feedback tools, and easy self-service options.",
+  },
+  {
+    img: "images/cctv-room.jpg",
+    title: "New Joiners: Intern Soye and Technical support Joel",
+    desc: "We welcome Soye (IT Intern) and Joel (Technical Support) to the NLCC IT team. Their onboarding has been approved to expand end-user support capabilities to improve response times & support coverage.",
+  },
+  {
+    img: "images/cybersec-img.jpg",
+    title: "🖥️ New CT-Box Integration with Aria",
+    desc: "The successful integration of the new CT-Box with Varian’s Aria platform enables faster image acquisition and more seamless treatment planning.",
+  },
+  {
+    img: "images/password-sec.jpg",
+    title: "📊 Revenue Assurance Middleware For NLCC Services",
+    desc: "To strengthen financial tracking, NLCC's IT is building a revenue assurance middleware that will sit between clinical services and the billing system.",
+  },
+  {
+    img: "images/VPN-ACCESS.jpg",
+    title: "📅 Survivorship Tracker",
+    desc: "The Survivorship Tracker is a custom-built module that consolidates post-treatment data for patients and supports automatic reporting.",
+  },
+  {
+    img: "images/prjector-img.jpg",
+    title: "💊 e-Chemo 2.0 : Smarter Prescriptions",
+    desc: "e-Chemo 2.0 now features task progress bars and faster regimen selection to reduce delays and improve control.",
+  },
+  {
+    img: "images/ai-portal.png",
+    title: "💻 Devices - Enterprise Scanners, Printers & Laptops",
+    desc: "Enterprise-grade printers, scanners & laptops have been deployed to enhance speed and reliability.",
+  },
+];
